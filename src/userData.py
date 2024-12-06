32917a9fa7e89c0aa53fcd014307a0f74991bf51
CHSXuan <42883077+CHSXuan@users.noreply.github.com>
2024-12-06 11:04:51 +0800

    Update userData.py

--- src/userData.py
+++ src/userData.py
@@ -1,12 +1,12 @@
 user_data = [{
-	'Phone': '13019006477',
-	'PassWord': 'lj3866235',
+	'Phone': '1111111',
+	'PassWord': '11111111',
 	'SendKey': 'SCT224063THyMSW5RUrdPQPc8OyFGu7Uky', # 用于微信通知, 在Server酱官网注册用户获取
 	'Address': '{"Reason":"","AttachmentFileName":"","LngLat":"130.3534,46.781403","Address":"黑龙江省佳木斯市向阳区红霞路345号"}' 
 },# 第二个人. 如果不需要, 只保留,号前的内容和最后的]号就行
 {
-	'Phone': '18931350097',
-	'PassWord': 'yileihjh0405',
+	'Phone': '1000000',
+	'PassWord': '6666666',
 	'SendKey': 'SCT224063THyMSW5RUrdPQPc8OyFGu7Uky',
 	'Address': '{"Reason":"","AttachmentFileName":"","LngLat":"130.3534,46.781403","Address":"黑龙江省佳木斯市向阳区红霞路345号"}' 
 }]
